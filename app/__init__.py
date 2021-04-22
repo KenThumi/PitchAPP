f03345c96866b0d87293302904d72032f63a4e87
Ken Thumi <kenthumi@gmail.com>
2021-04-23 02:17:51 +0300

    Added Blueprint main

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,9 @@ def create_app(config_name):
     #Creating the app configurations
     app.config.from_object(config_options[config_name]) 
 
+    #Registering Blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from app import app
+from . import main
 
-@app.route('/')
+@main.route('/')
 def home():
     return render_template('index.html')
