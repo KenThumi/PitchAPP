d03de44a417394a81781f3c24b49dd03b1af3c0c
Ken Thumi <kenthumi@gmail.com>
2021-04-23 02:00:32 +0300

    Initial setup

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import config_options
+
+
+
+
+
+def create_app(config_name):
+    '''Function to initialize app depending on various environment'''
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name]) 
+
+
+    return app
---
+++ config.py
@@ -0,0 +1,39 @@
+class Config:
+    '''General configuration parent class'''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    '''
+    Test  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
